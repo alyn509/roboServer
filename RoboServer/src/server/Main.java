67bfd23ab4c4b857875873129f7ea420433d4cd4
Alin-Marius <Alin-Marius@Rak>
2015-04-04 16:52:31 +0300

    listening html page

--- RoboServer/src/server/Main.java
+++ RoboServer/src/server/Main.java
@@ -1,12 +1,31 @@
 package server;
 
+import org.restlet.Server;
+import org.restlet.data.Protocol;
+import org.restlet.representation.Representation;
+import org.restlet.resource.*;
 
 
-public class Main {
 
-	public static void main(String[] args) {
+public class Main extends ServerResource {
+
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-
+		new Server(Protocol.HTTP, 8182, Main.class).start();
+		
+		/*		shit for adding stuff in colsole
+		new ClientResource("http://restlet.com").get().write(System.out);
+		*/
 	}
-
+/*
+    @Get("txt")
+    public String toString() {
+        return "hello, world";
+    }
+    */
+	
+    @Get("xml|html")
+    public Representation  represent() {
+        return new ClientResource("http://google.com").get();
+    }
 }
