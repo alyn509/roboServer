075336246e2c37d18dcc1fade8d912a25dddabc8
Alin-Marius <Alin-Marius@Rak>
2015-04-04 18:01:59 +0300

    html - working for stupid folder paths

--- RoboServer/src/server/Main.java
+++ RoboServer/src/server/Main.java
@@ -1,31 +1,23 @@
 package server;
 
 import org.restlet.Server;
+import org.restlet.data.MediaType;
 import org.restlet.data.Protocol;
+import org.restlet.representation.FileRepresentation;
 import org.restlet.representation.Representation;
 import org.restlet.resource.*;
 
 
 
 public class Main extends ServerResource {
-
+	
 	public static void main(String[] args) throws Exception {
-		// TODO Auto-generated method stub
+
 		new Server(Protocol.HTTP, 8182, Main.class).start();
-		
-		/*		shit for adding stuff in colsole
-		new ClientResource("http://restlet.com").get().write(System.out);
-		*/
 	}
-/*
-    @Get("txt")
-    public String toString() {
-        return "hello, world";
-    }
-    */
 	
     @Get("xml|html")
-    public Representation  represent() {
-        return new ClientResource("http://google.com").get();
+    public Representation represent() {
+        return new FileRepresentation("./html/thing.html", MediaType.TEXT_HTML);
     }
 }
